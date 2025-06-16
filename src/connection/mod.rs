53d18d6bc862aafa27b6ed8e2457d94285d2032a
2301_79425991 <2301_79425991@noreply.gitcode.com>
2025-06-16 23:07:28 +0800

    feat: define ConnectionManager

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -1,2 +1,16 @@
 mod manager;
 mod stream;
+mod tcp;
+mod udp;
+mod quic;
+mod websocket;
+mod buffer_pool;
+mod id;
+
+
+pub struct ConnectionConfig {
+    pub max_connections: usize,
+    pub connection_timeout_secs: u64,
+    pub write_buffer_size: usize,
+    pub tcp_keepalive_secs: Option<u32>,
+}
